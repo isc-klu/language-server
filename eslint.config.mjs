ad0f8ef367bd49a57c969adb644a42034e47253e
Kuang-Chen (KC) Lu <klu@intersystems.com>
2026-04-23 12:01:16 -0400

    save updated config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,40 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
 import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
-export default tseslint.config(
-    {
-        ignores: ["**/vscode.d.ts", "**/vscode.proposed.d.ts", "client/out/**", "server/out/**", "**/*.config.js"],
+export default defineConfig([
+  stylistic.configs.recommended,
+  tseslint.configs.recommended,
+  // tseslint.configs.stylistic,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
     },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-            ecmaVersion: 2018,
-            sourceType: "module",
-            parserOptions: {
-                server: "./server/tsconfig.json",
-                client: "./client/tsconfig.json",
-            },
-        },
-        rules: {
-            "@typescript-eslint/no-explicit-any": "off",
-        },
-    }
-);
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/jsonc",
+    languageOptions: {
+      allowTrailingCommas: true,
+    },
+  },
+  {
+    rules: {
+      // Stylistic
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+      "@stylistic/comma-dangle": ["error", "always-multiline"],
+      "@stylistic/max-statements-per-line": "off",
+      // TypeScript
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/prefer-for-of": "off",
+    },
+  },
+]);
