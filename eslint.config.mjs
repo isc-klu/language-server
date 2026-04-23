1aabf06f8977eda79b1ecd97665f8cc7be64850f
Kuang-Chen (KC) Lu <klu@intersystems.com>
2026-04-23 12:27:47 -0400

    new config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,39 +2,33 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
-import stylistic from "@stylistic/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  stylistic.configs.recommended,
-  tseslint.configs.recommended,
-  // tseslint.configs.stylistic,
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-    },
-  },
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    language: "json/jsonc",
-    languageOptions: {
-      allowTrailingCommas: true,
-    },
-  },
-  {
-    rules: {
-      // Stylistic
-      "@stylistic/semi": ["error", "always"],
-      "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
-      "@stylistic/comma-dangle": ["error", "always-multiline"],
-      "@stylistic/max-statements-per-line": "off",
-      // TypeScript
-      "@typescript-eslint/no-require-imports": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/prefer-for-of": "off",
-    },
-  },
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node },
+		},
+	},
+	{
+		files: ["**/*.json"],
+		plugins: { json },
+		language: "json/jsonc",
+		languageOptions: {
+			allowTrailingCommas: true,
+		},
+	},
+	tseslint.configs.recommended,
+	tseslint.configs.stylistic,
+	{
+		rules: {
+			"@typescript-eslint/no-require-imports": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-unused-expressions": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/prefer-for-of": "off",
+		},
+	},
+	eslintConfigPrettier,
 ]);
